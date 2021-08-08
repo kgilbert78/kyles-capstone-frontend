8592a30b31dc4a19892c11f733bbe3edc63cca4d
kgilbert78 <80863489+kgilbert78@users.noreply.github.com>
2021-08-07 20:15:40 -0400

    added all markers, attempted infoWindow

--- src/components/MainContent/MapPage/MapPage.js
+++ src/components/MainContent/MapPage/MapPage.js
@@ -1,137 +1,63 @@
-import "./MapPage.scss"
+// import googleMapsAPIKey from "./googleMapsAPIKey/googleMapsAPIKey";
 import { useState, useCallback, memo } from "react";
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
-export const MapPage = (props) => {
+// https://www.npmjs.com/package/@react-google-maps/api
 
-    // https://www.npmjs.com/package/@react-google-maps/api
+const containerStyle = {height: "calc(100vh - 50px)", width: "100vw", marginTop:50};
+const divStyle = {background: `white`, border: `1px solid #ccc`, padding: 15};
 
-    const containerStyle = {
-        height: '80vh',
-        width: '90vh'
-    };
+const center = {lat: 43.049736, lng:-76.150136};
 
-    const center = {lat: 43.0471677, lng: -76.1510856};
-
-    function WalkumentaryMap() {
-        const { isLoaded } = useJsApiLoader({
-            id: 'google-map-script',
-            googleMapsApiKey: "API key goes here"
-        });
-        
-        const [map, setMap] = useState(null);
-        
-        // https://reactjs.org/docs/hooks-reference.html#usecallback
-        const onLoad = useCallback(function callback(map) {
-            const bounds = new window.google.maps.LatLngBounds(); // omit window?
-            map.fitBounds(bounds);
-            // const directionsService= new google.maps.DirectionsService();
-            // const directionsRenderer = new google.maps.DirectionsRenderer();
-            // directionsRenderer.setMap(map);
-            setMap(map);
-        }, []);
-        
-        const onUnmount = useCallback(function callback(map) {
-            // directionsRenderer.setMap(null);
-            setMap(null);
-        }, []);
-        
-        return isLoaded ? (
-            <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={17} onLoad={onLoad} onUnmount={onUnmount}>
-                {            
-                    // // don't think I need 2 functions here. work on simplifying and still using button to call function.
-                    // const directionsButton = () => {
-                    //     //console.log("directions button is connected");
-                    //     calculateAndDisplayRoute(directionsService, directionsRenderer);
-                    // };
-
-                    // // const [directions, setDirections] = useState();
-                    // document.getElementById("getDirections").addEventListener('click', directionsButton);
-                
-                    // // convert to async/await
-                    // function calculateAndDisplayRoute(directionsService, directionsRenderer) {
-                    //     // make into array or object of site locations
-                    //     let youAreHere = '43.0472300, -76.1508000'; // temporarily fixed at Common Space
-                    //     let fayettePark = '43.048400, -76.146724';
-                    //     // // doesn't work formatted this way:
-                    //     // let youAreHere = new google.map.LatLng(43.0472300, -76.1508000);
-                    //     // let fayettePark = new google.map.LatLng(43.048400, -76.146724);
-                    //     directionsService.route({
-                    //         origin: youAreHere,
-                    //         destination: fayettePark,
-                    //         travelMode: google.maps.TravelMode.WALKING
-                    //     }).then((response) => {
-                    //         directionsRenderer.setDirections(response);
-                    //         // make error status variable work
-                    //     }).catch((error) => window.alert("Directions failed due to " + status));
-                    // };
-                
-                    // let youAreHere = new google.maps.Marker({
-                    //     // temporarily fixed at Common Space
-                    //     position: {lat: 43.0472300, lng: -76.1508000},
-                    //     map: map,
-                    //     label: "You are here"
-                    // });
-                
-                    // // for React, create addMarker function to pass objects into with props for coordinates and pop-up info windows
-                    // // make original marker disappear when marker that directions creates appears (keep popup somehow?)
-                    // const fayetteParkMarker = new google.maps.Marker({
-                    //     position: {lat: 43.048400, lng:-76.146724},
-                    //     map: map,
-                    //     label: "Fayette Park"
-                    // });
-                
-                    // const fayetteParkPopUp = new google.maps.InfoWindow({
-                    //     content: `<h1>Fayette Park</h1><p>Brief description</p>`
-                    // });
-                
-                    // fayetteParkMarker.addListener('click', () => {
-                    //     fayetteParkPopUp.open(map, fayetteParkMarker);
-                    // });
-                
-                    // const cityHallMarker = new google.maps.Marker({
-                    //     position: {lat: 43.050000, lng: -76.1490000},
-                    //     map: map,
-                    //     label: "City Hall"
-                    // });
-                
-                    // const cityHallPopUp = new google.maps.InfoWindow({
-                    //     content: `<h1>City Hall</h1><p>Brief description</p>`
-                    // });
-                
-                    // cityHallMarker.addListener('click', () => {
-                    //     cityHallPopUp.open(map, cityHallMarker);
-                    // });
-                }
-            </GoogleMap>
-        ) : <></>
-    };
+function WalkumentaryMap() {
+    const [map, setMap] = useState(null);
     
-    /*
-    NOTES:
-    
-    Example code:
-    https://developers.google.com/maps/documentation/javascript/examples/directions-simple#maps_directions_simple-html
-    
-    Example code for text directions:
-    https://developers.google.com/maps/documentation/javascript/examples/directions-panel
-    
-    */
+    // https://reactjs.org/docs/hooks-reference.html#usecallback
+    const onLoad = useCallback(function callback(map, infoWindow) {
+        setMap(map);
+        console.log('infoWindow: ', infoWindow);
+    }, []);
 
-    return (
-        <main className="mainContent">
-            <div id="top">
-                <h2>Google Maps Test</h2>
-                <button id="getDirections">Get Directions</button>
-            </div>
-            {/* <div id="map"></div> */}
-{/* 
-            <script
-                src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBrZIgybvj_BUwnQD2ndI2ExoBqf7DxAr0&callback=initMap&libraries=&v=weekly"
-                async
-            ></script> */}
-        </main>
-    );
+    // const onInfoWindowLoad = infoWindow => {
+    //     console.log('infoWindow: ', infoWindow)
+    // }
+    
+    const onUnmount = useCallback(function callback(map) {
+        setMap(null);
+    }, []);
+
+    //console.log(googleMapsAPIKey.key, typeof googleMapsAPIKey.key); // returns key w/o quotes, string but <LoadScript googleMapsApiKey={googleMapsAPIKey.key}> doesn't work
+    
+    return <LoadScript googleMapsApiKey="API key here">
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={17} 
+        onLoad={onLoad} 
+
+        
+        // onUnmount={onUnmount}
+        >
+           <Marker label="Fayette Park" position={{lat: 43.0484000, lng:-76.1467240}}></Marker>
+           {/* <InfoWindow onLoad={onLoad} position={{lat: 43.0484000, lng:-76.1467240}}>
+                <div style={divStyle}>
+                    <h1>Fayette Park</h1>
+                    <p>Brief description goes here.</p>
+                </div>
+            </InfoWindow> */}
+
+           {/* 
+           https://react-google-maps-api-docs.netlify.app/#infowindow 
+           anchor: MVCObject | undefined
+           Can be any MVCObject that exposes a LatLng position property and optionally a Point anchorPoint property for calculating the pixelOffset. The anchorPoint is the offset from the anchor's position to the tip of the InfoWindow.
+           
+           MVCObject takes addListener - might be able to use for pop up when marker is clicked
+           https://developers.google.com/maps/documentation/javascript/reference/event#MVCObject 
+           */}
+
+
+           <Marker label="City Hall / Market Hall" position={{lat: 43.0500000, lng: -76.1490000}}></Marker>
+           <Marker label="George Vashon Law Office" position={{lat: 43.0506402, lng:-76.1509000}}></Marker>
+           <Marker label="Jerry Rescue Monument?" position={{lat: 43.0507377, lng: -76.1534500}}></Marker>
+        </GoogleMap>
+        </LoadScript>
 };
 
-// export default memo(WalkumentaryMap);
+export const MapPage = memo(WalkumentaryMap);
