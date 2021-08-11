44dc5e58b7e2f98f4ca0dcf45bc55026e00df47c
kgilbert78 <80863489+kgilbert78@users.noreply.github.com>
2021-08-11 13:02:20 -0400

    audio buttons except volume working

--- src/components/MainContent/TourPage/BottomNav/BottomNav.js
+++ src/components/MainContent/TourPage/BottomNav/BottomNav.js
@@ -4,36 +4,56 @@ import "./BottomNav.scss"
 export const BottomNav = (props) => {
     const audioPlayer = useRef(null);
     const [filePath, setFilePath] = useState("/audio/freeSoundCanadianHorseAndCarriage.mp3");
+    const [playPauseButton, setPlayPauseButton] = useState("fas fa-play-circle");
 
-    // const [ audio, setAudio ] = useState(require('../../../../audio/freeSoundCanadianHorseAndCarriage.mp3'));
-    // const [ playing, setPlaying ] = useState(false);
-
-    // const playPause = () => setPlaying(!playing);
-
-    // useEffect(() => {
-    //     playing ? audio.pause() : audio.play();
-    // }, [playing]);
-
-    const playPause = ()=>{
-        if (audioPlayer.current.paused){
-        audioPlayer.current.play();
+    const playPause = () => {
+        if (audioPlayer.current.paused) {
+            audioPlayer.current.play();
+            setPlayPauseButton("fas fa-pause-circle");
         } else {
             audioPlayer.current.pause();
+            setPlayPauseButton("fas fa-play-circle");
+        };
+    };
+
+    const playFromStart = () => {
+        if (audioPlayer.current.play() || audioPlayer.current.pause()) {
+            audioPlayer.current.currentTime = 0;
         }
-    }
+    };
+
+    const rewind = () => {
+        if (audioPlayer.current.play() || audioPlayer.current.pause()) {
+            audioPlayer.current.currentTime -= 10.0;
+        }
+    };
+
+    const fastForward = () => {
+        if (audioPlayer.current.play() || audioPlayer.current.pause()) {
+            audioPlayer.current.currentTime += 10.0;
+        }
+    };
+
+    const volume = () => {
+        console.log("volume")
+        // if (audioPlayer.current.play()) {
+        //     // defaults to 1
+        //     // audioPlayer.current.volume += 0.1 // up
+        //     // audioPlayer.current.volume -= 0.1 // down
+        // }
+    };
 
     return (
         <div className="BottomNav">
             <nav className="navbarBottom navbar navbar-expand-lg navbar-light">
                 <div className="audioControls container-fluid justify-content-center">
-                    <a className="nav-link" href="#"><i className="fas fa-fast-backward"></i></a>
-                    <a className="nav-link" href="#"><i className="fas fa-backward"></i></a>
-                    {/* <a className="nav-link" href="#"><i className="fas fa-pause-circle"></i></a> */}
-                    {/* <a className="nav-link" href="#"><i className="fas fa-play-circle"></i></a> */}
-                    <a className="nav-link" href="#" onClick={playPause}><i className="fas fa-play-circle"></i></a>
-                    <a className="nav-link" href="#"><i className="fas fa-forward"></i></a>
-                    {/* <a className="nav-link" href="#"><i className="fas fa-fast-forward"></i></a> */}
-                    <a className="nav-link" href="#"><i className="fas fa-volume-up"></i></a>
+                    <a className="nav-link" href="#" onClick={playFromStart}><i className="fas fa-fast-backward"></i></a>
+                    <a className="nav-link" href="#" onClick={rewind}><i className="fas fa-backward"></i></a>
+                    <a className="nav-link" href="#" onClick={playPause}><i className={playPauseButton}></i></a>
+                    <a className="nav-link" href="#" onClick={fastForward}><i className="fas fa-forward"></i></a>
+                    <a className="nav-link volumeIcon" href="#" onClick={volume}>
+                        <i className="fas fa-volume-up"></i>
+                    </a>
                 </div>
                 <audio ref={audioPlayer}><source src={filePath} type="audio/mpeg"></source></audio>
             </nav>
