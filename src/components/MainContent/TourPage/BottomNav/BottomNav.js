fd8441b797228dbb0ba03223526457ef8b24fa82
kgilbert78 <80863489+kgilbert78@users.noreply.github.com>
2021-08-12 21:41:59 -0400

    volume works, vol# doesn't. Need to fix layout to integrate volume

--- src/components/MainContent/TourPage/BottomNav/BottomNav.js
+++ src/components/MainContent/TourPage/BottomNav/BottomNav.js
@@ -1,10 +1,11 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./BottomNav.scss"
 
 export const BottomNav = (props) => {
     const audioPlayer = useRef(null);
     const [filePath, setFilePath] = useState("/audio/freeSoundCanadianHorseAndCarriage.mp3");
     const [playPauseButton, setPlayPauseButton] = useState("fas fa-play-circle");
+    const [display, setDisplay] = useState();
 
     const playPause = () => {
         if (audioPlayer.current.paused) {
@@ -19,43 +20,94 @@ export const BottomNav = (props) => {
     const playFromStart = () => {
         if (audioPlayer.current.play() || audioPlayer.current.pause()) {
             audioPlayer.current.currentTime = 0;
-        }
+        };
     };
 
     const rewind = () => {
         if (audioPlayer.current.play() || audioPlayer.current.pause()) {
             audioPlayer.current.currentTime -= 10.0;
-        }
+        };
     };
 
     const fastForward = () => {
         if (audioPlayer.current.play() || audioPlayer.current.pause()) {
             audioPlayer.current.currentTime += 10.0;
-        }
+        };
     };
 
     const volume = () => {
         console.log("volume")
-        // if (audioPlayer.current.play()) {
-        //     // defaults to 1
-        //     // audioPlayer.current.volume += 0.1 // up
-        //     // audioPlayer.current.volume -= 0.1 // down
-        // }
+        // rename & use to toggle between displaying volumeControls & audioControls. Or put volumeControls in a dropdown(dropup?) menu like TopNav has?
     };
 
+    // add useEffect to make it refresh when the volume is adjusted so the right number displays. think about how useState for display/setDisplay should initially be set or interact with the useEffect.
+
+    const volumeDown = () => {
+        if (audioPlayer.current.play() && audioPlayer.current.volume > 0.11) {
+            // 0 to 1
+            audioPlayer.current.volume -= 0.1;
+            console.log(
+                "volumeDown", 
+                Math.round(audioPlayer.current.volume * 10)
+            );
+        };
+    };
+    const volumeUp = () => {
+        if (audioPlayer.current.play() && audioPlayer.current.volume < 0.9) {
+            // 0 to 1
+            audioPlayer.current.volume += 0.1;
+            console.log(
+                "volumeUp", 
+                Math.round(audioPlayer.current.volume * 10)
+            );
+        };
+    };
+
+/*
+    const audioControlsDiv = () => {
+        return (
+            <div className="audioControls container-fluid justify-content-center">
+                <span className="nav-link" href="#" onClick={playFromStart}><i className="fas fa-fast-backward"></i></span>
+                <span className="nav-link" href="#" onClick={rewind}><i className="fas fa-backward"></i></span>
+                <span className="nav-link" href="#" onClick={playPause}><i className={playPauseButton}></i></span>
+                <span className="nav-link" href="#" onClick={fastForward}><i className="fas fa-forward"></i></span>
+                <span className="nav-link volumeIcon" href="#" onClick={volume}>
+                    <i className="fas fa-volume-up"></i>
+                </span>
+            </div>
+        );
+    };
+
+    
+
+    const volumeControlsDiv = () => {
+        return (
+            <div>
+                <div className="volumeControls container-fluid justify-content-center">
+                    <span><i className="fas fa-minus-square fa-2x ms-5 me-5" onClick={volumeDown}></i></span>
+                    <h2>{Math.round(audioPlayer.current.volume * 10)}</h2>
+                    <span><i className="fas fa-plus-square fa-2x ms-5 me-5" onClick={volumeUp}></i></span>
+                </div>
+                <span><i className="fas fa-undo fa-lg me-2"></i></span>
+            </div>
+        );
+    };
+*/
     return (
         <div className="BottomNav">
             <nav className="navbarBottom navbar navbar-expand-lg navbar-light">
-                <div className="audioControls container-fluid justify-content-center">
-                    <a className="nav-link" href="#" onClick={playFromStart}><i className="fas fa-fast-backward"></i></a>
-                    <a className="nav-link" href="#" onClick={rewind}><i className="fas fa-backward"></i></a>
-                    <a className="nav-link" href="#" onClick={playPause}><i className={playPauseButton}></i></a>
-                    <a className="nav-link" href="#" onClick={fastForward}><i className="fas fa-forward"></i></a>
-                    <a className="nav-link volumeIcon" href="#" onClick={volume}>
-                        <i className="fas fa-volume-up"></i>
-                    </a>
-                </div>
-                <audio ref={audioPlayer}><source src={filePath} type="audio/mpeg"></source></audio>
+            
+            <div className="audioControls container-fluid justify-content-center">
+                <span className="nav-link" href="#" onClick={playFromStart}><i className="fas fa-fast-backward"></i></span>
+                <span className="nav-link" href="#" onClick={rewind}><i className="fas fa-backward"></i></span>
+                <span className="nav-link" href="#" onClick={playPause}><i className={playPauseButton}></i></span>
+                <span className="nav-link" href="#" onClick={fastForward}><i className="fas fa-forward"></i></span>
+                <span className="nav-link volumeIcon" href="#" onClick={volume}>
+                    <i className="fas fa-volume-up"></i>
+                </span>
+            </div>
+
+            <audio ref={audioPlayer}><source src={filePath} type="audio/mpeg"></source></audio>
             </nav>
         </div>
     );
