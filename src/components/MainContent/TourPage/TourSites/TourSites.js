2f97291ce0a85fb65dad098feb87cb7925776d32
kgilbert78 <80863489+kgilbert78@users.noreply.github.com>
2021-08-20 21:07:00 -0400

    DB data ddisplaying except setting initial featureImage

--- src/components/MainContent/TourPage/TourSites/TourSites.js
+++ src/components/MainContent/TourPage/TourSites/TourSites.js
@@ -4,15 +4,13 @@ import { BottomNav } from "../BottomNav/BottomNav";
 import { RichText } from "./RichText";
 
 export const TourSites = (props) => {
-    const [pageData, setPageData] = useState(null); //{}
-    const [audioFilePath, setAudioFilePath] = useState("/audio/fayetteParkNarration.mp3");
-    // const [site, setSite] = useState(null);
-    // const [location, setLocation] = useState(null);
-    // const [photos, setPhotos] = useState(null);
-    // const [soundEffects, setSoundEffects] = useState(null);
-    // const [textCredits, setTextCredits] = useState(null);
+    const [siteData, setSiteData] = useState({});
+    const [featureImageSource, setFeatureImageSource] = useState([]);
+    //const [featureImageSource, setFeatureImageSource] = useState('/images/hotelSyracuse.jpg');
 
-    //console.log("from Tour page:", props.fetchParams)
+    const [audioFilePath, setAudioFilePath] = useState("/audio/fayetteParkNarration.mp3");
+
+    // console.log("Tour page fetch:", props.fetchParams) // prints multiples
 
     useEffect(() => {
         const loadSite = async () => {
@@ -21,188 +19,153 @@ export const TourSites = (props) => {
                 method: "GET"
             });
             const data = await response.json();
-            setPageData(data.selectedSiteData[0]);
-            // console.log(pageData.name, pageData.photos[0].url);
-            // setSite(data.selectedSiteData[0]);
-            // setPhotos(data.selectedSiteData[0].photos)
+            //console.log(data)
+            setSiteData(data.selectedSiteData[0]);
+            setFeatureImageSource(photos?.url?.[0]);
         };
         loadSite();
+        console.log(featureImageSource)
     },[]);
+   
+    const {site, location, photos, soundEffects, textCredits} = siteData;
 
-    // console.log(pageData.name, pageData.photos[0].url);
-
-    const hotelSyracuseNow = '/images/hotelSyracuse.jpg';
-    //const initialImage = pageData.photos[0].url;
-
-    const [featureImageSource, setFeatureImageSource] = useState(hotelSyracuseNow);
-
-    // const Thumbnails = () => {
-    //     pageData.photos.map((photo) => {
-    //         return (
-    //             <div key={photo.photoID} className="thumbnailImageDiv col-3">
-    //                 <img 
-    //                     id={photo.photoID}
-    //                     className="thumbnailImage img-thumbnail rounded"
-    //                     src={photo.url} 
-    //                     alt={photo.altTag}
-    //                     onClick={()=>{
-    //                         setFeatureImageSource(photo.url)
-    //                     }}
-    //                 />
-    //             </div>
-    //         );
-    //     });
-    // };
-    
-    if (pageData === null) {
+    if (siteData === null || location === null || photos=== null || soundEffects=== null || textCredits=== null) {
         return <h1>Loading...</h1>
     }
-        return (
-            <div className="TourSite">
-                <main>
-                    <div className="mainContent container-fluid d-flex justify-content-around flex-wrap">
-                        <section className="imagesSounds col-lg-5">
-                            <h1 className="siteName pb-2">
-                                {pageData.name}
-                            </h1>
-                        {/* NEED TO GIVE CREDIT FOR IMAGE! source & license requirements at https://commons.wikimedia.org/wiki/File:Hotel_Syracuse,_New_York.jpg */}
-                        <div className="imageContainer">
-                                <img 
-                                className="featureImage img-fluid rounded" 
-                                id="featureImage"
-                                // src={pageData.photos[0].url}
-                                src={featureImageSource}
-                                alt="Hotel Syracuse"
-                                />
-                        </div> 
-                        
-                            <h2 className="featureImageInfo text-center pt-3">
-                                Year of Picture / relevant info
-                                {/* {pageData.photos[0].caption} */}
-                            </h2>
+    
+    
+    return (
+        <div className="TourSite">
+            <main>
+                <div className="mainContent container-fluid d-flex justify-content-around flex-wrap">
+                    <section className="imagesSounds col-lg-5">
+                        <h1 className="siteName pb-2">
+                            {siteData.name}
+                        </h1>
 
-                            <div id="thumbnailRow" className="thumbnailRow row pt-3">
-                            {/* <div id="thumbnailRow" className="thumbnailRow row pt-3" onClick={changeFeatureImage(event)}> */}
+                    <div className="imageContainer">
+                            <img 
+                            className="featureImage img-fluid rounded" 
+                            id="featureImage"
+                            src={featureImageSource}
+                            alt="Hotel Syracuse"
+                            />
+                    </div> 
+                    
+                        <h2 className="featureImageInfo text-center pt-3">
+                            {/* Captions need to change with images */}
+                            {photos?.[0]?.caption}
+                        </h2>
 
-                                {/* <Thumbnails /> */}
+                        <div id="thumbnailRow" className="thumbnailRow row pt-3">
 
-
-                                <div className="thumbnailImageDiv col-3">
+                        {photos?.map((photo) => {
+                            return (
+                                <div key={photo?.photoID} className="thumbnailImageDiv col-3">
                                     <img 
-                                        id="thumbnailImage1"
+                                        id={photo?.photoID}
                                         className="thumbnailImage img-thumbnail rounded"
-                                        src={'/images/hotelSyracuse.jpg'} 
-                                        alt="Hotel Syracuse today thumbnail"
+                                        src={photo?.url} 
+                                        alt={photo?.altTag}
                                         onClick={()=>{
-                                            setFeatureImageSource('/images/hotelSyracuse.jpg')
+                                            setFeatureImageSource(photo?.url)
                                         }}
                                     />
                                 </div>
-                                <div className="thumbnailImageDiv col-3">
-                                    {/*  NEED TO GIVE CREDIT FOR IMAGE! source & license requirements at https://commons.wikimedia.org/wiki/File:Hotel-syracuse_1920s.jpg */}
-                                    <img 
-                                        id="thumbnailImage2"
-                                        className="thumbnailImage img-thumbnail rounded" 
-                                        src={"/images/hotelSyracuse1920s.jpg"}
-                                        alt="Hotel Syracuse 1920s thumbnail"
-                                        onClick={()=>{
-                                            setFeatureImageSource("/images/hotelSyracuse1920s.jpg")
-                                        }}
-                                    />
-                                </div>
-                                <div className="thumbnailImageDiv col-3">
-                                    <img 
-                                        id="thumbnailImage3"
-                                        className="thumbnailImage img-thumbnail rounded" 
-                                        src={'/images/hotelSyracuse1920s.jpg'}
-                                        alt="Hotel Syracuse thumbnail 3"
-                                        onClick={()=>{
-                                            setFeatureImageSource("/images/hotelSyracuse1920s.jpg")
-                                        }}
-                                    />
-                                </div>
-                                <div className="thumbnailImageDiv col-3">
-                                    <img 
-                                        id="thumbnailImage4"
-                                        className="thumbnailImage img-thumbnail rounded" 
-                                        src={'/images/hotelSyracuse1920s.jpg'}
-                                        alt="Hotel Syracuse thumbnail 4"
-                                        onClick={()=>{
-                                            setFeatureImageSource("/images/hotelSyracuse1920s.jpg")
-                                        }}
-                                    />
-                                </div>
-
-                                
-                            </div> 
-
-                            <div className="soundEffects container-fluid d-flex justify-content-between pt-4">
-                                <div>
-                                    <button 
-                                        type="button" 
-                                        className="btn btn-lg btn-info"
-                                        onClick={(event) => {
-                                            setAudioFilePath("/audio/fayetteParkNarration.mp3")
-                                            console.log("audio in Tour page", audioFilePath)
-                                        }}
-                                    >
-                                        Play Narrative
-                                    </button>
-                                </div>
-                                <div>
-                                    <button 
-                                        type="button" 
-                                        className="btn btn-lg btn-info"
-                                        onClick={(event) => {
-                                            setAudioFilePath("/audio/frederickDouglass")
-                                            console.log("audio in Tour page", audioFilePath)
-                                        }}
-                                    >
-                                        Douglass Speech
-                                        {/* {pageData.soundEffects[0].name} */}
-                                    </button>
-                                </div>
-                                <div>
-                                <button 
-                                        type="button" 
-                                        className="btn btn-lg btn-info"
-                                        onClick={(event) => {
-                                            setAudioFilePath("/audio/freeSoundCanadianHorseAndCarriage")
-                                            console.log("audio in Tour page", audioFilePath)
-                                        }}
-                                    >
-                                        Horse & Carriage
-                                        {/* {pageData.soundEffects[1].name} */}
-                                    </button>
-                                </div>
-                            </div>
-                            <div className="soundEffectCaptions mt-3 text-center">
-                                <p>Sound effect details go here.</p>
-                            </div>
+                            );
+                        })}
                             
-                        </section>
 
-                        <section className="siteInfo col-lg-5">
-                            <h1 className="subtitle pt-3 m-0">
-                                About the Hotel Syracuse
-                                {/* About {pageData.name} */}
-                            </h1>
-                            <div className="description">
-                                <p>text goes here</p>
-                                {/* <RichText text={pageData.text} /> */}
-                                {/* install DOMPurify: https://www.npmjs.com/package/dompurify  */}
-                                <p>SOURCES:<br />
-                                    <em>
-                                        Sources go here, with links.<br />
-                                        {/* use .map to loop over them & add <br /> after each */}
-                                    </em>
-                                </p>
+                            
+                        </div> 
+
+                        <div className="soundEffects container-fluid d-flex justify-content-between pt-4">
+                            <div>
+                                <button 
+                                    type="button" 
+                                    className="btn btn-lg btn-info"
+                                    onClick={(event) => {
+                                        setAudioFilePath("/audio/fayetteParkNarration.mp3")
+                                        console.log("audio in Tour page", audioFilePath)
+                                    }}
+                                >
+                                    Play Narrative
+                                </button>
                             </div>
-                        </section>
-                    </div>
-                </main>
-                <BottomNav audioFilePath={audioFilePath} />
-            </div>
-        )
-    // }
+                            <div>
+                                <button 
+                                    type="button" 
+                                    className="btn btn-lg btn-info"
+                                    onClick={(event) => {
+                                        setAudioFilePath("/audio/frederickDouglass")
+                                        
+                                            console.log("audio in Tour page", audioFilePath)
+                                    
+                                    }}
+                                >
+                                    {/* Douglass Speech */}
+                                    {soundEffects?.[0]?.name}
+                                </button>
+                            </div>
+                            <div>
+                            <button 
+                                    type="button" 
+                                    className="btn btn-lg btn-info"
+                                    onClick={(event) => {
+                                        event.preventDefault();
+                                        setAudioFilePath("/audio/freeSoundCanadianHorseAndCarriage")
+                                        console.log("audio in Tour page", audioFilePath)
+                                    }}
+                                >
+                                    {/* Horse & Carriage */}
+                                    {soundEffects?.[1]?.name}
+                                    {/* {console.log(soundEffects?.[1]?.name)}  */}
+                                    {/* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining */}
+                                </button>
+                            </div>
+                        </div>
+                        <div className="soundEffectCaptions mt-3 text-center">
+                            <ul>
+                                {soundEffects?.map((soundEffect) => {
+                                    // console.log(soundEffect?.caption)
+                                    return (
+                                        soundEffect.caption ? 
+                                            <li key={soundEffect?.soundEffectID}>{soundEffect?.caption}</li>
+                                        : null
+                                    );
+                                })}
+                            </ul> 
+                        </div>
+                        
+                    </section>
+
+                    <section className="siteInfo col-lg-5">
+                        <h1 className="subtitle pt-3 m-0">
+                            About {siteData.name}
+                        </h1>
+                        <div className="description">
+                            <RichText text={siteData.text} />
+                            {/* install DOMPurify: https://www.npmjs.com/package/dompurify  */}
+                            <p>SOURCES:<br />
+                                <em>
+                                    {
+                                        textCredits?.map((textCredit) => {
+                                            return (
+                                                <p key={textCredit?.textCreditID} className="my-0">
+                                                    {/* {textCredit?.credit} */}
+                                                    <a href={textCredit?.url !== "" ? textCredit?.url : null}>{textCredit?.credit}</a>
+                                                </p>
+                                            )
+                                        })
+                                    }
+                                </em>
+                            </p>
+                        </div>
+                    </section>
+                </div>
+            </main>
+            <BottomNav audioFilePath={audioFilePath} />
+        </div>
+    )
+
 };
