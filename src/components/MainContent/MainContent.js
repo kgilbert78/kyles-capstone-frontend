73e7db34a23ab5b8e904c333c731b9baf854b26d
kgilbert78 <80863489+kgilbert78@users.noreply.github.com>
2021-12-06 22:40:10 -0500

    removed recursive map approach

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -3,7 +3,6 @@ import { Homepage } from "./Homepage/Homepage";
 import { MapPage } from "./MapPage/MapPage";
 import { TourSites } from "./TourPage/TourSites/TourSites";
 import { SiteList } from "./SiteList/SiteList";
-import { SiteListRec } from "./SiteList/SiteListRec";
 
 
 export const MainContent = (props) => {
@@ -22,9 +21,6 @@ export const MainContent = (props) => {
                     </Route>
                     <Route path="/sites">
                         <SiteList />
-                    </Route>
-                    <Route path="/sites2">
-                        <SiteListRec />
                     </Route>
                 </Switch>
             </Router>
