06dc9602cf8d76263ddeadb899acf30b92d0c270
kgilbert78 <kgilbert78@gmail.com>
2025-01-18 20:46:18 -0500

    add heroku key

--- src/components/MainContent/MapPage/MapPage.js
+++ src/components/MainContent/MapPage/MapPage.js
@@ -75,7 +75,7 @@ function WalkumentaryMap() {
             <h3>Please show your location, then click a sneaker for directions to a site!</h3>
             <button className="btn btn-info mb-3" onClick={getUserLocation}>Show my location</button>
         </div>
-        <LoadScript googleMapsApiKey={process.env.REACT_APP_API_KEY_NETLIFY}>
+        <LoadScript googleMapsApiKey="AIzaSyARi7HP7yVVLDHdHSSkVPW_Deu_EyjdbkI">
             <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={16}
                 onLoad={onLoad}
             >
